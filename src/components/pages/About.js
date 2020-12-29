0e84ccad145e808a3828bd0a84700675f2ecf34b
alex-tenczar <alex-desktop@pop-os.localdomain>
2020-12-29 15:46:31 -0500

    additional styling changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,7 +7,7 @@ function About () {
         <div id="About">
             <p>📍 concord, nh:</p><Clock format={'LTS'} ticking={true} timezone={'America/New_York'} />
             <p id="bio">Hi. I'm an Information Technology graduate student at the <a href="https://manchester.unh.edu/">University of New Hampshire at Manchester</a>. Over the past couple years I've become especially interested in pursuing web development. I enjoy using Python and React to make my ideas come to life. I'm currently working freelance as a front-end web developer as I finish up my final year of school.</p>
-            <a id="resume">🗎 resume</a>
+            <a id="resume">resume</a>
         </div>
     )
 };
