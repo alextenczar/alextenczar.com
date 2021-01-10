e1c86e2b8fbb7a294bd017c64af4fe0ac1f00ee6
alex-tenczar <alex-desktop@pop-os.localdomain>
2021-01-10 16:28:30 -0500

    updated favicon, added resume

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,14 @@
 import '../../scss/App.scss';
 import Clock from 'react-live-clock';
-import '../../scss/about.scss'
+import '../../scss/about.scss';
+import resume from '../../static/ResumeAlexTenczar2021.pdf';
 
 function About () {
     return (
         <div id="About">
             <p>📍 Concord, NH, USA:</p><Clock format={'LTS'} ticking={true} timezone={'America/New_York'} />
             <p id="bio">Hi. I'm an Information Technology graduate student at the <a href="https://manchester.unh.edu/">University of New Hampshire at Manchester</a>. Over the past few years I've become especially interested in pursuing web development. I enjoy using Python and React to make my ideas come to life. I'm currently working as a freelance front-end web developer while I complete my final year of school.</p>
-            <a id="resume">resume</a>
+            <a id="resume" href={resume}>resume</a>
         </div>
     )
 };
