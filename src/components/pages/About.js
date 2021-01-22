f7c8e822cac7823bb88d0e68803afff95ef79356
alex-tenczar <alex-desktop@pop-os.localdomain>
2021-01-21 20:36:27 -0500

    new tab for pdf

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ function About () {
         <div id="About">
             <p>📍 Concord, NH, USA:</p><Clock format={'LTS'} ticking={true} timezone={'America/New_York'} />
             <p id="bio">Hi. I'm an Information Technology graduate student at the <a href="https://manchester.unh.edu/" target="_blank" rel="noopener noreferrer">University of New Hampshire at Manchester</a>. Over the past few years I've become especially interested in pursuing web development. I enjoy using Python and React to make my ideas come to life. I'm currently working as a freelance front-end web developer while I complete my final year of school.</p>
-            <a id="resume" href={resume}>resume</a>
+            <a id="resume" href={resume} target="_blank" rel="noopener noreferrer">resume</a>
         </div>
     )
 };
