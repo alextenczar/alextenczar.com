8e8b0fc86a721682ed9c134f2b7f0ae72c0029f9
alextenczar <alextenczar@gmail.com>
2021-05-31 21:23:19 -0400

    Updated about and portfolios

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,6 +3,7 @@ import '../../scss/projects.scss';
 import {ReactComponent as Linkicon} from '../../static/external-link.svg';
 import { SRLWrapper } from "simple-react-lightbox";
 import djangoproject from '../../static/django-project.png';
+import bandviz from '../../static/bandviz.gif';
 import cpproject from '../../static/cp-project.png';
 
 const options = {
@@ -30,6 +31,16 @@ function Projects () {
     return (
     <SRLWrapper options={options}>
         <div id="Projects">
+        <div className="project">
+                <div className="project-text">
+                    <a href="https://github.com/alextenczar/bandviz.com" className="project-link" target="_blank" rel="noopener noreferrer">BandViz.com - A Web Application for Visualizing Music Discovery <Linkicon className="link-icon"/></a>
+                    <p><strong>Recipient of UNHM MSIT Program's 2020-2021 Master’s Project Award.</strong><br></br> For my master's project I created a React web application that utilizes Last.fm and Spotify's APIs to visualize similar artists a user searches for. The project was deployed to <a href="https://www.bandviz.com" target="_blank" rel="noopener noreferrer">Vercel</a> to be accessed from any device.</p>
+                    <p><strong>Technologies used:</strong> React.js, D3.js, Axios, SASS, Vercel, RESTful APIs.</p>
+                </div>
+                <div className="project-image">
+                    <img id="bandviz_gif" src={bandviz} alt="BandViz.com"></img>
+                </div>
+            </div>
             <div className="project">
                 <div className="project-text">
                     <a href="https://github.com/alextenczar/3-julian-alex" className="project-link" target="_blank" rel="noopener noreferrer">Django Spreadsheet Web App <Linkicon className="link-icon"/></a>
